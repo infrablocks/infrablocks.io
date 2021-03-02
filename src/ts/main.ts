f0769f3b49a1637befd0608073514f5709c8c4d9
Toby Clemson <tobyclemson@gmail.com>
2021-03-02 02:13:43 +0000

    Extract feedback button handling into typescript. Disable buttons after feedback provided.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,42 @@
-import '../css/main.css';
+import '../css/main.css'
 
-function printMessage(message: string) {
-  console.log(`Message: ${message}`)
+enum Feedback {
+  Helpful = "helpful",
+  Unhelpful = "unhelpful"
 }
 
-printMessage("How about now?");
+function sendFeedbackEvent(feedback: Feedback) {
+  gtag('event', 'docs_feedback', {
+    'feedback': feedback
+  })
+}
 
+function disableFeedbackWidget() {
+  $(".feedback-button")
+    .removeClass("bg-gray-200")
+    .addClass("bg-gray-100")
+  $(".feedback-button > a")
+    .removeClass("text-gray-700")
+    .addClass(["text-gray-400", "cursor-default"])
+  $("#feedback-buttons")
+    .off("click", "a#feedback-helpful")
+    .off("click", "a#feedback-unhelpful")
+}
+
+function feedbackClickHandler(feedback: Feedback) {
+  return function (event: Event) {
+    event.preventDefault()
+    sendFeedbackEvent(feedback)
+    disableFeedbackWidget()
+  }
+}
+
+$.when($.ready).then(function () {
+  $("#feedback-buttons")
+    .on("click",
+      "a#feedback-helpful",
+      feedbackClickHandler(Feedback.Helpful))
+    .on("click",
+      "a#feedback-unhelpful",
+      feedbackClickHandler(Feedback.Unhelpful))
+})
