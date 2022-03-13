9549b04404980f547bd403595f3707d1cadb95d2
Toby <tobyclemson@gmail.com>
2022-03-13 00:31:29 +0000

    Stop using sudo in CI script.

--- scripts/ci/common/install-git-crypt.sh
+++ scripts/ci/common/install-git-crypt.sh
@@ -4,5 +4,5 @@
 set -e
 set -o pipefail
 
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends git ssh git-crypt
+apt-get update
+apt-get install -y --no-install-recommends git ssh git-crypt
