727dd790074fb910416cd2c2985f2151891355f7
Toby <tobyclemson@gmail.com>
2022-03-13 00:32:15 +0000

    Stop using sudo in CI script.

--- scripts/ci/common/install-aws-cli.sh
+++ scripts/ci/common/install-aws-cli.sh
@@ -6,6 +6,6 @@ set -o pipefail
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install
+./aws/install
 
 /usr/local/bin/aws --version
